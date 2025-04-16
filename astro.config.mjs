92f1e3535b2b9fb600fc465724abb51a82ab6829
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-04-17 00:49:15 +0530

    prettier working; project config updates;

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: "http://localhost:4321",
+    outDir: './build',
     prefetch: {
         prefetchAll: true
     },
